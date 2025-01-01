b307f5c652fdcf5efc2ff3701b51a29b9bfd78fc
Eray <112173428+erayefekutlu@users.noreply.github.com>
2025-01-01 21:06:56 +0300

    Update script.js

--- script.js
+++ script.js
@@ -7,11 +7,11 @@ function fetchDepremVerileri() {
         const listItem = document.createElement("li");
         listItem.textContent = `${deprem.title} - Büyüklük: ${deprem.mag}`;
         if (deprem.mag <= 2.50) {
-            listItem.classList.add('yesil');
+          listItem.classList.add('yesil');
         } else if (deprem.mag < 5) {
-            listItem.classList.add('turuncu');
+          listItem.classList.add('turuncu');
         } else {
-            listItem.classList.add('kirmizi');
+          listItem.classList.add('kirmizi');
         }
 
         let isInfoDivVisible = false;
@@ -23,13 +23,22 @@ function fetchDepremVerileri() {
           } else {
             infoDiv = document.createElement("div");
             let rawDate = deprem.date;
-            let date = new Date(rawDate);
-            if (isNaN(date.getTime())) {
-              rawDate = rawDate.replace(/-/g, "/").replace("T", " ").replace("Z", "");
-              date = new Date(rawDate);
+
+            try {
+              const date = new Date(rawDate); // ISO formatı
+              if (isNaN(date.getTime())) {
+                const { parseISO, format } = dateFns;
+                const parsedDate = parseISO(rawDate);
+                const formattedDate = format(parsedDate, "dd/MM/yyyy HH:mm:ss");
+                infoDiv.textContent = `Tarih ve Saat: ${formattedDate} Derinlik: ${deprem.depth} - Enlem: ${deprem.geojson.coordinates[1]} - Boylam: ${deprem.geojson.coordinates[0]}`;
+              } else {
+                infoDiv.textContent = "Tarih formatı desteklenmiyor.";
+              }
+            } catch (error) {
+              console.error("Tarih dönüşümünde hata:", error);
+              infoDiv.textContent = "Tarih formatıyla ilgili sorun var.";
             }
-            let formattedDate = formatDate(date);
-            infoDiv.textContent = `Tarih ve Saat: ${formattedDate} Derinlik: ${deprem.depth} - Enlem: ${deprem.geojson.coordinates[1]} - Boylam: ${deprem.geojson.coordinates[0]}`;
+
             infoDiv.style.display = "none";
             this.appendChild(infoDiv);
             infoDiv.style.display = "block";
@@ -40,15 +49,3 @@ function fetchDepremVerileri() {
       });
     });
 }
-
-function formatDate(date) {
-  let day = ('0' + date.getDate()).slice(-2);
-  let month = ('0' + (date.getMonth() + 1)).slice(-2);
-  let year = date.getFullYear();
-  let hours = ('0' + date.getHours()).slice(-2);
-  let minutes = ('0' + date.getMinutes()).slice(-2);
-  let seconds = ('0' + date.getSeconds()).slice(-2);
-  return `${day}/${month}/${year} ${hours}:${minutes}:${seconds}`;
-}
-
-fetchDepremVerileri();
